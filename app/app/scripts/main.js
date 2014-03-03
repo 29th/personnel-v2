37868095c78c0b5d91c12231a7a2bf927067b2cb
wilson29thid <wilson@29th.org>
2014-03-03 12:19:28 +0000

    Fixed validation bootstrap integration

--- app/app/scripts/main.js
+++ app/app/scripts/main.js
@@ -129,18 +129,22 @@ window.requirejs = window.requirejs || {};
         // Modify input validation to work with bootstrap
         _.extend(Backbone.Validation.callbacks, {
             valid: function (view, attr, selector) {
-                var $el = view.$('[name=' + attr + ']'), 
-                    $group = $el.closest('.form-group');
+                var $el = view.$("[name=" + attr + "]"), 
+                    $group = $el.closest(".form-group"),
+                    $msgContainer = $group.find(".error-msg");
+                if( ! $msgContainer.length) $msgContainer = $("<div/>").addClass("help-block").addClass("error-msg").appendTo($group);
                 
-                $group.removeClass('has-error');
-                $group.find('.help-block').html('').addClass('hidden');
+                $group.removeClass("has-error");
+                $msgContainer.html("").addClass("hidden");
             },
             invalid: function (view, attr, error, selector) {
-                var $el = view.$('[name=' + attr + ']'), 
-                    $group = $el.closest('.form-group');
+                var $el = view.$("[name=" + attr + "]"), 
+                    $group = $el.closest(".form-group"),
+                    $msgContainer = $group.find(".error-msg");
+                if( ! $msgContainer.length) $msgContainer = $("<div/>").addClass("help-block").addClass("error-msg").appendTo($group);
                 
-                $group.addClass('has-error');
-                $group.find('.help-block').html(error).removeClass('hidden');
+                $group.addClass("has-error");
+                $msgContainer.html(error).removeClass("hidden");
             }
         });
 
