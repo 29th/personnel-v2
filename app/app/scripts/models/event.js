2376534a7cb353ea35b28fabd21d04142bc0b292
Wilson29thID <wilson@29th.org>
2014-11-21 23:55:14 -0500

    Parse whether event is mandatory

--- app/app/scripts/models/event.js
+++ app/app/scripts/models/event.js
@@ -7,10 +7,14 @@ define([
     "use strict";
 
     return Backbone.Model.extend({
+        initialize: function() {
+            var mandatory = this.get("mandatory");
+        },
         url: function () {
             return config.apiHost + "/events/" + this.id;
         },
         parse: function (response, options) {
+            if(response.event) response.event.mandatory = parseInt(response.event.mandatory, 10);
             return response.event || {};
         }
         /*,excuse: function(excused) {
