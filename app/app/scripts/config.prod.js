bce3151d3a44fd501abad43bb9cbfc33f29abe1c
Wilson29thID <wilson@29th.org>
2014-11-21 21:15:29 -0500

    Changed coat dir

--- app/app/scripts/config.prod.js
+++ app/app/scripts/config.prod.js
@@ -1,8 +1,8 @@
 define(function () {
     return {
         baseUrl: "http://personnel.29th.org",
-        apiHost: "http://api.29th.org",
-        coatDir: "http://29th.org/coats",
+        apiHost: "/personnel-api",
+        coatDir: "http://api.29th.org/coats",
         forumUrl: "http://forums.29th.org",
         vanillaCategoryEnlistments: 62
     };
