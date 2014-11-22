c1df82dcd7da3812d187b1570e5444feb215361f
Wilson29thID <wilson@29th.org>
2014-11-21 21:17:46 -0500

    Fixed prod api url

--- app/app/scripts/config.prod.js
+++ app/app/scripts/config.prod.js
@@ -1,7 +1,7 @@
 define(function () {
     return {
         baseUrl: "http://personnel.29th.org",
-        apiHost: "/personnel-api",
+        apiHost: "http://api.29th.org",
         coatDir: "http://api.29th.org/coats",
         forumUrl: "http://forums.29th.org",
         vanillaCategoryEnlistments: 62
