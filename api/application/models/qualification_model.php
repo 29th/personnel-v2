d130a4a3315db9610699f438f15dfe5e57aed305
Wilson29thID <wilson@29th.org>
2014-12-01 21:52:58 -0500

    Separated qualifications and standards

--- api/application/models/qualification_model.php
+++ api/application/models/qualification_model.php
@@ -5,12 +5,15 @@ class Qualification_model extends CRUD_Model {
     public $primary_key = 'qualifications.id';
     
     public function default_select() {
-        $this->db->select('qualifications.id, qualifications.standard_id, qualifications.date, qualifications.author_member_id')
-            ->select('s.weapon AS `standard|weapon`, s.badge AS `standard|badge`, s.description AS `standard|description`');
+        $this->db->select('qualifications.id, qualifications.date')
+            ->select('qualifications.author_member_id AS `author|id`, ' . $this->virtual_fields['short_name'] . ' AS `author|short_name`', FALSE)
+            ->select('qualifications.standard_id AS `standard|id`, s.weapon AS `standard|weapon`, s.badge AS `standard|badge`, s.description AS `standard|description`');
     }
     
     public function default_join() {
-        $this->db->join('standards AS s', 's.id = qualifications.standard_id');
+        $this->db->join('standards AS s', 's.id = qualifications.standard_id')
+            ->join('members', 'members.id = qualifications.author_member_id')
+            ->join('ranks', 'ranks.id = members.rank_id', 'left');
     }
     
     public function order_by() {
