cc8089d3ba0790af6cfe2b57e6b763af7afb9ea3
Wilson29thID <wilson@29th.org>
2014-12-05 23:03:26 -0500

    Added qualification date and author

--- app/app/scripts/views/member_qualifications.js
+++ app/app/scripts/views/member_qualifications.js
@@ -10,57 +10,6 @@ define([
     "marionette"
 ], function ($, _, Backbone, Qualification, GameTemplate, WeaponTemplate, BadgeTemplate, StandardTemplate) {
     
-    /*return Backbone.Marionette.ItemView.extend({
-        template: Template,
-        initialize: function (options) {
-            options = options || {};
-            this.qualifications = options.qualifications  || {};
-            this.permissions = options.permissions  || {};
-            this.memberPermissions = options.memberPermissions  || {};
-            //this.awards = options.awards || false;
-        },
-        serializeData: function() {
-            var permissions = this.permissions.length ? this.permissions.pluck("abbr") : [],
-                memberPermissions = this.memberPermissions.length ? this.memberPermissions.pluck("abbr") : [],
-                qualifications = this.qualifications,
-                sortedStandards = {},
-                allowedTo = {
-                    addQualification: memberPermissions.indexOf("qualification_add") !== -1 || permissions.indexOf("qualification_add_any") !== -1
-                };
-            _.each(this.collection.toJSON(), function(standard) {
-                // Set default values so objects don't break
-                if( ! standard.game) standard.game = "N/A";
-                if( ! standard.weapon) standard.weapon = "N/A";
-                if( ! standard.badge) standard.badge = "N/A";
-                
-                // Make sure dimensions exists
-                if(sortedStandards[standard.game] === undefined) {
-                    sortedStandards[standard.game] = {};
-                }
-                if(sortedStandards[standard.game][standard.weapon] === undefined) {
-                    sortedStandards[standard.game][standard.weapon] = {};
-                }
-                if(sortedStandards[standard.game][standard.weapon][standard.badge] === undefined) {
-                    sortedStandards[standard.game][standard.weapon][standard.badge] = [];
-                }
-                
-                // Check if member has qualified for this standard
-                var qualified = qualifications.filter(function(qualification) { return qualification.get("standard").id === standard.id; });
-                if(qualified.length) {
-                    standard.qualification = qualified[0].toJSON();
-                }
-                
-                // Add this standard to its group
-                sortedStandards[standard.game][standard.weapon][standard.badge].push(standard);
-            });
-            
-            return {
-                standards: sortedStandards,
-                allowedTo: allowedTo
-            };
-        }
-    });*/
-    
     var StandardView = Backbone.Marionette.ItemView.extend({
         template: StandardTemplate,
         initialize: function(options) {
@@ -84,9 +33,14 @@ define([
             "change": "render" // Coulda sworn marionette did this out of the box...
         },
         serializeData: function() {
+            var model = this.model.toJSON();
+            
+            // Convert qualification model to JSON since .toJSON() is not recursive
+            if(model.qualification !== undefined) model.qualification = model.qualification.toJSON();
+            
             return _.extend({
                 allowedTo: this.rootOptions.allowedTo
-            }, this.model.toJSON());
+            }, model);
         },
         onToggleQualification: function(e) {
             var self = this;
