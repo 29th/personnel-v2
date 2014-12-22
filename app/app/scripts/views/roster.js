398c7655dfb8f7698a558aa9e2e8dd9c388e6f3a
Wilson29thID <wilson@29th.org>
2014-12-21 22:48:31 -0500

    Nestable roster

--- app/app/scripts/views/roster.js
+++ app/app/scripts/views/roster.js
@@ -4,14 +4,17 @@ define([
     "backbone",
     "marionette",
     "handlebars",
-    "hbs!templates/roster_collapsible",
+    "hbs!templates/roster_nestable",
+    "hbs!templates/roster_nestable_container",
     "hbs!templates/roster_attendance",
     "marionette"
-], function ($, _, Backbone, Marionette, Handlebars, Template, AttendanceTemplate) {
+], function ($, _, Backbone, Marionette, Handlebars, Template, TemplateContainer, AttendanceTemplate) {
     
     var ItemView = Backbone.Marionette.CompositeView.extend({
         template: Template,
-        tagName: "div",
+        tagName: "li",
+        className: "dd-item",
+        itemViewContainer: "ol",
         initialize: function (options) {
             options = options || {};
             this.itemViewOptions = this.itemViewOptions || {};
@@ -24,20 +27,23 @@ define([
                 this.template = AttendanceTemplate;
                 this.itemViewOptions.attendance = true;
             }
-            this.collection = this.model.get("children");
-            this.$el.addClass("unit").addClass("depth-" + this.model.get("depth"));
+            if(this.model.get("children").length) {
+                this.collection = this.model.get("children");
+            }
+            //this.$el.addClass("unit").addClass("depth-" + this.model.get("depth"));
+            this.$el.attr("data-id", this.model.get("id"));
         },
         /*events: {
             "click .header:first": "onClickHeader"
         },*/
-        appendHtml: function (collectionView, itemView) {
+        /*appendHtml: function (collectionView, itemView) {
             // Different append method for attendance roster
             if(this.itemViewOptions.attendance) {
                 this.$el.append(itemView.el);
             } else {
                 collectionView.$(".children:first").append(itemView.el);
             }
-        },
+        },*/
         onBeforeRender: function () {
             // If eventAttendance is set, add attended and excused values to each member
             if (!_.isEmpty(this.eventAttendance)) {
@@ -59,15 +65,33 @@ define([
         }*/
     });
 
-    return Backbone.Marionette.CollectionView.extend({
+    return Backbone.Marionette.CompositeView.extend({
         itemView: ItemView,
         title: "Roster",
-        className: "roster",
+        className: "roster dd",
+        template: TemplateContainer,
+        itemViewContainer: "ol",
         initialize: function (options) {
             options = options || {};
             this.itemViewOptions = this.itemViewOptions || {};
             if (options.attendance) this.itemViewOptions.attendance = true;
             if (options.eventAttendance) this.itemViewOptions.eventAttendance = options.eventAttendance;
+            _.bindAll(this, "onClickControls");
+        },
+        onRender: function() {
+            this.$el.nestable();
+        },
+        events: {
+            "click .panel-ctrls a": "onClickControls"
+        },
+        onClickControls: function(e) {
+            var action = $(e.currentTarget).attr('data-action');
+            if(action === "expand") {
+                this.$el.nestable("expandAll");
+            } else {
+                this.$el.nestable("collapseAll");
+            }
+            e.preventDefault();
         }
     });
 });
