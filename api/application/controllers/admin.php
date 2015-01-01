57fa9c1dbfec8479897b8a90a64fda1869715a13
Wilson29thID <wilson@29th.org>
2015-01-01 16:37:03 -0500

    Added game to admin/units

--- api/application/controllers/admin.php
+++ api/application/controllers/admin.php
@@ -406,8 +406,8 @@ class Admin extends CI_Controller {
 	public function units()
 	{
 	    $this->grocery_crud->set_table('units')
-	        ->columns('name', 'abbr', 'path', 'order', 'timezone', 'class', 'active')
-	        ->fields('id', 'name', 'abbr', 'path', 'order', 'timezone', 'class', 'active')
+	        ->columns('name', 'abbr', 'path', 'order', 'game', 'timezone', 'class', 'active')
+	        ->fields('id', 'name', 'abbr', 'path', 'order', 'game', 'timezone', 'class', 'active')
 	        ->required_fields('name', 'abbr', 'path', 'class')
 	        ->display_as('abbr', 'Abbreviation');
         $output = $this->grocery_crud->render();
