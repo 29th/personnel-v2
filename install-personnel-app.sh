a9143107ab9e0b2770f4a4064d430a71a3ba95c9
wilson29thid <wilson@29th.org>
2015-01-07 07:37:54 -0500

    Re-enabled installing front-end dependencies

--- install-personnel-app.sh
+++ install-personnel-app.sh
@@ -3,7 +3,7 @@ DIR_CWD=$1
 echo 'Installing personnel app...'
 git clone https://github.com/29th/personnel-app.git ${DIR_CWD}personnel-app
 
-#echo 'Installing personnel front-end dependencies...'
-#(cd ${DIR_CWD}personnel-app && \
-#    npm install && \
-#    ./node_modules/bower/bin/bower install --allow-root)
+echo 'Installing personnel front-end dependencies...'
+(cd ${DIR_CWD}personnel-app && \
+    npm install && \
+    ./node_modules/bower/bin/bower install --allow-root)
