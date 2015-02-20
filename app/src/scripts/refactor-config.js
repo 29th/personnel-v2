577a3b3593473fa8804b26ce4c2e911bc7c4eb65
Wilson29thID <wilson@29th.org>
2015-02-19 23:07:49 -0500

    Removed unnecessary files

--- app/src/scripts/refactor-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	quote           :  '"',         // '\'' or '"'
-	style           :  'comma',        // 'var', 'comma', 'comma-first'
-	indent          :  2,            // the tab size used in your project
-	directoryFilter :  null,         // not supported yet
-	fileFilter      :  '.js',        // the extension of the file to upgrade
-	dryrun          :  false         // true|false if true no changes will be written to upgraded files
-};
--- app/src/scripts/require-refactor.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	quote: '"',
-	style: 'comma',
-	indent: 5,
-	directoryFilter: null,
-	fileFilter: '.js',
-	dryrun: true
-}
