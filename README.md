c5541b177b1d71a036f226d283efee06e89da25b
wilson29thid <wilson@29th.org>
2015-03-21 22:09:12 +0000

    Updated README, use sed for personnel-app env var config

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Personnel Setup
 
-1. Create an account on [Cloud 9](http://c9.io)
-2. Create a new workspace that is a clone of this repository
-3. Using the terminal, run `source install.sh $IP $C9_USER`
-4. After installation completes, turn on Cloud 9's web server by clicking **Run > Run With > Apache httpd**
-5. Navigate to the `/repositories/forums` path provided in the installation success message
-6. On the Vanilla installer, change the **Database Host** from `localhost` to `0.0.0.0` and fill in an admin email, username, and password
-7. It may bring you back to the installer screen afterwards, but if there's no error, you're good to go. Navigate to `/repositories/forums` again to verify you're logged in
-8. Navigate to `/repositories/personnel-app/dist` to view the application
+1. Go to the following repositories and click **Fork** in the top-right corner
+    1. [29th/personnel-api](https://github.com/29th/personnel-api)
+    2. [29th/personnel-app](https://github.com/29th/personnel-app)
+    3. [29th/vanilla](https://github.com/29th/vanilla)
+2. Create an account on [Cloud 9](http://c9.io) using GitHub to authenticate (or create a new login and authenticate with GitHub on the dashb0ard)
+3. Click **Create new workspace** and select **Clone from URL**
+4. Use the clone URL found on the right-hand side of this repository and click **Create**
+5. Using the terminal at the bottom of the workspace, run the following command, replacing `YOUR_GITHUB_USERNAME` with your GitHub username:
+```bash
+$ source install.sh YOUR_GITHUB_USERNAME
+```
+6. When the installation finishes, you'll get an **Installation complete!** message with a URL to navigate to. Copy that into a new browser tab to navigate to it. (Note: Make sure `:80` is not added to the URL after you paste it)
+7. On the Vanilla installer, change the **Database Host** from `localhost` to `0.0.0.0` and fill in an admin email, username, and password (Note: It may bring you back to the installer screen afterwards, but if there's no error, you're good to go.)
+8. Navigate to the **Installation complete!** URL again to verify you're logged in
+9. Navigate to `https://<your site url>/repositories/personnel-app/dist` to view the application
--- scripts/configure-env-vars.sh
+++ scripts/configure-env-vars.sh
@@ -7,27 +7,26 @@ VANILLA_COOKIE_SALT=$(sudo cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
 echo 'Configuring personnel-api environment variables'
 cp ${REPOSITORIES}personnel-api/.env.sample ${REPOSITORIES}personnel-api/.env
 sed -i ${REPOSITORIES}personnel-api/.env \
-    -e "s/DB_DEFAULT_HOSTNAME=.*/DB_DEFAULT_HOSTNAME=${DB_HOSTNAME}/" \
-    -e "s/DB_DEFAULT_USERNAME=.*/DB_DEFAULT_USERNAME=${DB_USERNAME}/" \
-    -e "s/DB_FORUMS_HOSTNAME=.*/DB_FORUMS_HOSTNAME=${DB_HOSTNAME}/" \
-    -e "s/DB_FORUMS_USERNAME=.*/DB_FORUMS_USERNAME=${DB_USERNAME}/" \
-    -e "s/ENCRYPTION_KEY=.*/ENCRYPTION_KEY=${ENCRYPTION_KEY}/" \
-    -e "s/VANILLA_COOKIE_SALT=.*/VANILLA_COOKIE_SALT=${VANILLA_COOKIE_SALT}/"
+    -e "s,DB_DEFAULT_HOSTNAME=.*,DB_DEFAULT_HOSTNAME=${DB_HOSTNAME}," \
+    -e "s,DB_DEFAULT_USERNAME=.*,DB_DEFAULT_USERNAME=${DB_USERNAME}," \
+    -e "s,DB_FORUMS_HOSTNAME=.*,DB_FORUMS_HOSTNAME=${DB_HOSTNAME}," \
+    -e "s,DB_FORUMS_USERNAME=.*,DB_FORUMS_USERNAME=${DB_USERNAME}," \
+    -e "s,ENCRYPTION_KEY=.*,ENCRYPTION_KEY=${ENCRYPTION_KEY}," \
+    -e "s,VANILLA_COOKIE_SALT=.*,VANILLA_COOKIE_SALT=${VANILLA_COOKIE_SALT},"
 
 echo 'Configuring personnel-app environment variables'
-export BASE_URL=${REPOSITORIES}personnel-app
-export API_HOST=${REPOSITORIES}personnel-api
-export COAT_DIR=${REPOSITORIES}personnel-api/coats
-export FORUM_VANILLA_BASE_URL=${REPOSITORIES}forums
-export FORUM_SMF_BASE_URL=http://29th.org/forums
-export WIKI_URL=http://29th.org/wiki
+cp ${REPOSITORIES}personnel-app/.env.sample ${REPOSITORIES}personnel-app/.env
+sed -i ${REPOSITORIES}personnel-app/.env \
+    -e "s,API_HOST=.*,API_HOST=/${REPOSITORIES}personnel-api," \
+    -e "s,COAT_DIR=.*,COAT_DIR=/${REPOSITORIES}personnel-api/coats," \
+    -e "s,FORUM_VANILLA_BASE_URL=.*,FORUM_VANILLA_BASE_URL=/${REPOSITORIES}forums," \
 
-echo 'Configuring forums variables'
+echo 'Configuring forums environment variables'
 cp ${REPOSITORIES}forums/conf/config.sample.php ${REPOSITORIES}forums/conf/config.php
 chmod 777 ${REPOSITORIES}forums/conf/config.php
 sed -i ${REPOSITORIES}forums/conf/config.php \
-    -e "s/\$Configuration\['Database'\]\['Host'\] = '.*'/\$Configuration\['Database'\]\['Host'\] = '${DB_HOSTNAME}'/" \
-    -e "s/\$Configuration\['Database'\]\['User'\] = '.*'/\$Configuration\['Database'\]\['User'\] = '${DB_USERNAME}'/" \
-    -e "s/\$Configuration\['Debug'\] = FALSE/\$Configuration\['Debug'\] = TRUE/" \
-    -e "s/\$Configuration\['Garden'\]\['Cookie'\]\['Salt'\] = '.*'/\$Configuration\['Garden'\]\['Cookie'\]\['Salt'\] = '${VANILLA_COOKIE_SALT}'/" \
+    -e "s,\$Configuration\['Database'\]\['Host'\] = '.*',\$Configuration\['Database'\]\['Host'\] = '${DB_HOSTNAME}'," \
+    -e "s,\$Configuration\['Database'\]\['User'\] = '.*',\$Configuration\['Database'\]\['User'\] = '${DB_USERNAME}'," \
+    -e "s,\$Configuration\['Debug'\] = FALSE,\$Configuration\['Debug'\] = TRUE," \
+    -e "s,\$Configuration\['Garden'\]\['Cookie'\]\['Salt'\] = '.*',\$Configuration\['Garden'\]\['Cookie'\]\['Salt'\] = '${VANILLA_COOKIE_SALT}'," \
     -e "s,\$Configuration\['Plugins'\]\['AddMenuitem'\]\['Link4'\] = '.*',\$Configuration\['Plugins'\]\['AddMenuitem'\]\['Link4'\] = '${REPOSITORIES}personnel-app/dist',"
