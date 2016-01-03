83a9feab2172ddb92e602e7536e7aa42e6eefc35
swomma <swomma@swomma.pl>
2016-01-03 20:56:53 +0000

    CABs alerts added

--- api/application/models/alerts_model.php
+++ api/application/models/alerts_model.php
@@ -9,7 +9,10 @@ class Alerts_model extends MY_Model {
           "SQL_CALC_FOUND_ROWS `members`.`id` AS `member|id`, " . 
           "CONCAT(ranks.`abbr`, ' ', IF(members.`name_prefix` != '', CONCAT(members.`name_prefix`, ' '), ''), members.`last_name`) AS `member|short_name`, " .
           "COALESCE(`aocc_awardings`.`aocc_count`, 0) AS `aocc_count`, ".
-          "COALESCE(`ww1v_awardings`.`ww1v_count`, 0) AS `ww1v_count`"
+          "COALESCE(`ww1v_awardings`.`ww1v_count`, 0) AS `ww1v_count`, ".
+          "COALESCE(`cabs_awardings`.`cab_lvl`, 0) AS `cab_lvl`, ".
+          "COALESCE(`recuits`.`rec_cnt`, 0) AS `rec_cnt`, ".
+          "last_enl_date"
         , FALSE); // SQL_CALC_FOUND_ROWS allows a COUNT after the query
     }
 
@@ -17,6 +20,8 @@ class Alerts_model extends MY_Model {
         $this->filter_join('ranks', 'ranks.id = members.rank_id', 'left');
         $this->filter_join('(SELECT member_id, count(1) AS aocc_count FROM awardings WHERE awardings.award_id = 10 GROUP BY awardings.member_id) AS aocc_awardings', 'aocc_awardings.member_id = members.id', 'left');
         $this->filter_join('(SELECT member_id, count(1) AS ww1v_count FROM awardings WHERE awardings.award_id = 61 GROUP BY awardings.member_id) AS ww1v_awardings', 'ww1v_awardings.member_id = members.id', 'left');
+        $this->filter_join('(SELECT member_id, Max(award_id)-27 AS cab_lvl FROM awardings WHERE awardings.award_id IN (28,29,30,31,32) GROUP BY awardings.member_id) AS cabs_awardings', 'cabs_awardings.member_id = members.id', 'left');
+        $this->filter_join('(SELECT recruiter_member_id AS id, Count(1) as rec_cnt, Max(date) AS last_enl_date FROM enlistments WHERE status="Accepted" AND recruiter_member_id IS NOT NULL GROUP BY recruiter_member_id) AS recuits', 'recuits.id = members.id', 'left');
     }
 
     public function default_order_by($member_id = FALSE) {
