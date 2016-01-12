570524e7a688f02ebe9c24f994a564430645a656
swomma <swomma@swomma.pl>
2016-01-12 01:04:46 +0000

    Notes - viewing only

---
+++ app/src/scripts/views/member_notes.js
@@ -0,0 +1,22 @@
+var $ = require("jquery"),
+  _ = require("underscore"),
+  Backbone = require("backbone"),
+  Template = require("../templates/member_notes.html"),
+  config = require("../config");
+var Marionette = require("backbone.marionette");
+
+  
+  module.exports = Marionette.ItemView.extend({
+      template: Template,
+      initialize: function(options) {
+          options = options || {};
+          this.notes = options.collection || false;
+      },
+      serializeData: function () {
+          var items = [];
+          return _.extend({
+              notes: this.notes.toJSON(),
+              count: this.notes.length
+          });
+      }
+  });
