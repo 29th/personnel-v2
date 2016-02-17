e883c9f96623f29ff52cfa2a2fbaadff6f7db155
wilson29thid <wilson@29th.org>
2016-02-16 19:08:41 -0500

    increment forum version to 2.1.11

--- scripts/install-vanilla.sh
+++ scripts/install-vanilla.sh
@@ -2,7 +2,7 @@ DIR_CWD=$1
 GITHUB_USER=${2-29th}
 
 echo 'Installing Vanilla...'
-git clone --recursive -b 29th-extensions-2.1.8p2 https://github.com/${GITHUB_USER}/vanilla.git ${DIR_CWD}forums
+git clone --recursive -b 29th-extensions-2.1.11 https://github.com/${GITHUB_USER}/vanilla.git ${DIR_CWD}forums
 #(cd ${DIR_CWD}forums && git checkout 29th-extensions && git submodule update --init --recursive)
 
 if [ "$GITHUB_USER" != "29th" ]; then
