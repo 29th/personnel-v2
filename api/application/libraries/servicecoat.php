e1e6730f4eb4c663f8eef37ec09d12b048114531
swomma <swomma@swomma.pl>
2016-07-04 09:47:49 +0000

    Service Coat generator: added badges for Grenadier AIT

--- api/application/libraries/servicecoat.php
+++ api/application/libraries/servicecoat.php
@@ -22,9 +22,9 @@ class ServiceCoat {
 			'm:rifle:ro2','m:bar:ro2','m:zook:ro2','m:mg:ro2','m:smg:ro2','m:sniper:ro2',
 			's:rifle:ro2','s:bar:ro2','s:zook:ro2','s:mg:ro2','s:smg:ro2','s:sniper:ro2',
 			'e:rifle:ro2','e:bar:ro2','e:zook:ro2','e:mg:ro2','e:smg:ro2','e:sniper:ro2',
-			'm:rifle:a3','m:bar:a3','m:zook:a3','m:mg:a3','m:sniper:a3','m:armor:a3','m:smg:a3',
-			's:rifle:a3','s:bar:a3','s:zook:a3','s:mg:a3','s:sniper:a3','s:armor:a3','s:smg:a3',
-			'e:rifle:a3','e:bar:a3','e:zook:a3','e:mg:a3','e:sniper:a3','e:armor:a3','e:smg:a3'
+			'm:rifle:a3','m:bar:a3','m:zook:a3','m:mg:a3','m:sniper:a3','m:armor:a3','m:smg:a3','m:grenadier:a3',
+			's:rifle:a3','s:bar:a3','s:zook:a3','s:mg:a3','s:sniper:a3','s:armor:a3','s:smg:a3','s:grenadier:a3',
+			'e:rifle:a3','e:bar:a3','e:zook:a3','e:mg:a3','e:sniper:a3','e:armor:a3','e:smg:a3','e:grenadier:a3'
 			);
 		private $scAllARibbons = array('french','ww1v','aocc','eamc','acamp','adef','gcon','aach','arcom','anpdr','pheart','bstar','sm','lom','sstar','dsm','dsc','ww2v','dms','movsm','arcam');
 		private $scAllURibbons = array('dh','dod','trenches','battlegrounds','muc','rs','arma');
@@ -391,6 +391,7 @@ class ServiceCoat {
 				'mg' => NULL,
 				'zook' => NULL,
 				'armor' => NULL,
+				'grenadier' => NULL,
 				'sniper' => NULL,
 				'mortar' => NULL
 				);
@@ -465,6 +466,9 @@ class ServiceCoat {
 					break;
 					case 'armor':
 						$scClaspsList['armor'] = imagecreatefrompng(getenv('DIR_COAT_RESOURCES') . 'BadgesAQB/armor_clasps.png');
+					break;
+					case 'grenadier':
+						$scClaspsList['grenadier'] = imagecreatefrompng(getenv('DIR_COAT_RESOURCES') . 'BadgesAQB/grenadier_clasps.png');
 					break;
 					case 'sniper':
 						$scClaspsList['sniper'] = imagecreatefrompng(getenv('DIR_COAT_RESOURCES') . 'BadgesAQB/sniper_clasps.png');
