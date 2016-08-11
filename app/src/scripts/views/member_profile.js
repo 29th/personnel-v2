f1d9973b5b982b87f3f0dab9cc876f76d9a801fd
swomma <swomma@swomma.pl>
2016-08-11 11:42:16 +0000

    Link to signature in API response for member

--- app/src/scripts/views/member_profile.js
+++ app/src/scripts/views/member_profile.js
@@ -13,31 +13,12 @@ var Marionette = require("backbone.marionette");
           this.finances = options.finances || false;
       },
       serializeData: function () {
-          var fin_sum = _.reduce(this.finances.pluck("amount_received"), function(m, x) { return +m + +x; }, 0),
-              sig = '',
-              steam_id = this.model.toJSON().steam_id;
+          var fin_sum = _.reduce(this.finances.pluck("amount_received"), function(m, x) { return +m + +x; }, 0);
           
-//          var a = this.sigExists( config.sigUrl + '/' + steam_id + '.png' );
-          $.ajax({
-            url:  config.sigUrl + '/' + steam_id + '.png',
-            type: "GET",
-            async:false,
-            dataType: "json",
-            success: function() {
-              sig = this.url;
-            }
-          })
           return _.extend({
               forum: config.forum,
               fin_sum: fin_sum,
-              sig: config.sigUrl + '/' + steam_id + '.png',
               short_name_url: this.model.get("short_name").replace("/", "")
           }, this.model.toJSON());
-      },
-      sigExists(src) {
-        var img = new Image();
-        img.onload = function() {return src;};
-        img.onerror = function() { return '';};
-        img.src = src;
       }
   });
