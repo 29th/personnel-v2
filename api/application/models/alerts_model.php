83d0fe1154c4b186bf7ddd83bfcc8e951b09ec49
swomma <swomma@swomma.pl>
2016-11-21 00:32:56 +0000

    Fix for AOCC alerts for GDed members

--- api/application/models/alerts_model.php
+++ api/application/models/alerts_model.php
@@ -8,8 +8,9 @@ class Alerts_model extends MY_Model {
         $this->db->select(
           "SQL_CALC_FOUND_ROWS `members`.`id` AS `member|id`, " . 
           "CONCAT(ranks.`abbr`, ' ', IF(members.`name_prefix` != '', CONCAT(members.`name_prefix`, ' '), ''), members.`last_name`) AS `member|short_name`, " .
-          "COALESCE(`aocc_awardings`.`aocc_count`, 0) AS `aocc_count`, ".
-          "COALESCE(`ww1v_awardings`.`ww1v_count`, 0) AS `ww1v_count`, ".
+          "0 as aocc_count, 0 AS ww1v_count, ".
+          "COALESCE(`aocc_awardings`.`aocc_list`, '') AS `aocc_list`, ".
+          "COALESCE(`ww1v_awardings`.`ww1v_list`, '') AS `ww1v_list`, ".
           "COALESCE(`cabs_awardings`.`cab_lvl`, 0) AS `cab_lvl`, ".
           "COALESCE(`recuits`.`rec_cnt`, 0) AS `rec_cnt`, ".
           "last_enl_date"
@@ -18,8 +19,8 @@ class Alerts_model extends MY_Model {
 
     public function default_join($member_id = FALSE) {
         $this->filter_join('ranks', 'ranks.id = members.rank_id', 'left');
-        $this->filter_join('(SELECT member_id, count(1) AS aocc_count FROM awardings WHERE awardings.award_id = 10 GROUP BY awardings.member_id) AS aocc_awardings', 'aocc_awardings.member_id = members.id', 'left');
-        $this->filter_join('(SELECT member_id, count(1) AS ww1v_count FROM awardings WHERE awardings.award_id = 61 GROUP BY awardings.member_id) AS ww1v_awardings', 'ww1v_awardings.member_id = members.id', 'left');
+        $this->filter_join('(SELECT member_id, GROUP_CONCAT(date) AS aocc_list FROM awardings WHERE awardings.award_id = 10 GROUP BY awardings.member_id) AS aocc_awardings', 'aocc_awardings.member_id = members.id', 'left');
+        $this->filter_join('(SELECT member_id, GROUP_CONCAT(date) AS ww1v_list FROM awardings WHERE awardings.award_id = 61 GROUP BY awardings.member_id) AS ww1v_awardings', 'ww1v_awardings.member_id = members.id', 'left');
         $this->filter_join('(SELECT member_id, Max(award_id)-27 AS cab_lvl FROM awardings WHERE awardings.award_id IN (28,29,30,31,32) GROUP BY awardings.member_id) AS cabs_awardings', 'cabs_awardings.member_id = members.id', 'left');
         $this->filter_join('(SELECT recruiter_member_id AS id, Count(1) as rec_cnt, Max(date) AS last_enl_date FROM enlistments WHERE status="Accepted" AND recruiter_member_id IS NOT NULL GROUP BY recruiter_member_id) AS recuits', 'recuits.id = members.id', 'left');
     }
