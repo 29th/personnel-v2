09b61c5b945edc588578d90e94fa7f0bc609f923
wilson29thid <wilson@29th.org>
2020-01-04 22:48:05 +0000

    Set error reporting to -1 with displaying off in prod

--- api/index.php
+++ api/index.php
@@ -40,11 +40,15 @@ if (defined('ENVIRONMENT'))
 	{
 		case 'development':
 			error_reporting(E_ALL);
+			ini_set('display_errors', 1);
+			ini_set('display_startup_errors', 1);
 		break;
 	
 		case 'testing':
 		case 'production':
-			error_reporting(0);
+			error_reporting(-1);
+			ini_set('display_errors', 0);
+			ini_set('display_startup_errors', 0);
 		break;
 
 		default:
