bfd03f1eb679dcc521d6f115dcf0f2923cd92730
wilson29thid <wilson@29th.org>
2020-01-05 14:04:44 +0000

    Disable error reporting in prod (against medical advice) See https://codeigniter.com/userguide2/general/errors.html

--- api/index.php
+++ api/index.php
@@ -46,7 +46,7 @@ if (defined('ENVIRONMENT'))
 	
 		case 'testing':
 		case 'production':
-			error_reporting(-1);
+			error_reporting(0);
 			ini_set('display_errors', 0);
 			ini_set('display_startup_errors', 0);
 		break;
